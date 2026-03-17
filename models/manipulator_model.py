06c3596c18787a9271826b0b75607f02be91abff
shhhQuiettt <krzysztof.skro@gmail.com>
2026-03-17 13:50:10 +0100

    flc and model for m3!=0

--- models/manipulator_model.py
+++ models/manipulator_model.py
@@ -1,6 +1,7 @@
 from math import cos, sin
 import numpy as np
 
+
 def almost_equal(a, b, tol=1e-5):
     return abs(a - b) < tol
 
@@ -9,40 +10,48 @@ class ManiuplatorModel:
     def __init__(self, Tp):
         self.Tp = Tp
 
+        self.m1 = 3
         self.l1 = 0.5
         self.r1 = 0.04
-        self.m1 = 3
         self.I_1 = 1 / 12 * self.m1 * (3 * self.r1**2 + self.l1**2)
         self.d1 = self.l1 / 2
         _expected_I_1 = 0.06370
-        assert almost_equal(self.I_1, _expected_I_1), f"Expected I_1 to be approximately {_expected_I_1}, but got {self.I_1}"
+        assert almost_equal(
+            self.I_1, _expected_I_1
+        ), f"Expected I_1 to be approximately {_expected_I_1}, but got {self.I_1}"
 
+        self.m2 = 2.4
         self.l2 = 0.4
         self.r2 = 0.04
-        self.m2 = 2.4
         self.I_2 = 1 / 12 * self.m2 * (3 * self.r2**2 + self.l2**2)
         self.d2 = self.l2 / 2
         _expected_I_2 = 0.03296
-        assert almost_equal(self.I_2, _expected_I_2), f"Expected I_2 to be approximately {_expected_I_2} but got {self.I_2}"
+        assert almost_equal(
+            self.I_2, _expected_I_2
+        ), f"Expected I_2 to be approximately {_expected_I_2} but got {self.I_2}"
 
-        self.m3 = 0.0
+        self.m3 = 10
         self.r3 = 0.05
         self.I_3 = 2.0 / 5 * self.m3 * self.r3**2
-        _expected_I_3 = 0.0
-        assert almost_equal(self.I_3, _expected_I_3), f"Expected I_3 to be approximately {_expected_I_3}, but got {self.I_3}"
+        _expected_I_3 = 0.01000
+        assert almost_equal(
+            self.I_3, _expected_I_3
+        ), f"Expected I_3 to be approximately {_expected_I_3}, but got {self.I_3}"
 
         self.alpha = (
             self.m1 * self.d1**2
             + self.I_1
             + self.m2 * (self.l1**2 + self.d2**2)
             + self.I_2
-            # + self.m3 * (self.l1**2 + self.l2**2)
             + self.I_3
         )
 
         self.beta = self.m2 * self.l1 * self.d2
-        self.gamma = self.m2*self.d2**2 + self.I_2 + self.I_3
+        self.gamma = self.m2 * self.d2**2 + self.I_2 + self.I_3
 
+        self.au = self.m3 * (self.l1**2 + self.l2**2)
+        self.eu = self.m3 * self.l2**2
+        self.ly = self.m3 * self.l1 * self.l2
 
     def M(self, x):
         """
@@ -50,15 +59,13 @@ class ManiuplatorModel:
         (2DoF planar manipulator with the object at the tip)
         """
         q1, q2, q1_dot, q2_dot = x
-        
-        m11 = self.alpha + 2 * self.beta * cos(q2)
-        m12 = self.gamma + self.beta * cos(q2)
+
+        m11 = self.alpha + 2 * self.beta * cos(q2) + self.au + 2 * self.ly * cos(q2)
+        m12 = self.gamma + self.beta * cos(q2) + self.eu + self.ly * cos(q2)
         m21 = m12
-        m22 = self.gamma
+        m22 = self.gamma + self.eu
 
         return np.array([[m11, m12], [m21, m22]])
-
-
 
     def C(self, x):
         """
@@ -67,9 +74,11 @@ class ManiuplatorModel:
         """
         q1, q2, q1_dot, q2_dot = x
 
-        c11 = -self.beta * sin(q2) * q2_dot
-        c12 = -self.beta * sin(q2) * (q1_dot + q2_dot)
-        c21 = self.beta * sin(q2) * q1_dot
+        k = self.beta + self.ly
+
+        c11 = -k * sin(q2) * q2_dot
+        c12 = -k * sin(q2) * (q1_dot + q2_dot)
+        c21 = k * sin(q2) * q1_dot
         c22 = 0.0
 
         return np.array([[c11, c12], [c21, c22]])
