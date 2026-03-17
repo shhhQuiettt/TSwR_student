b1eebd4caa23d28a1f7c25623f474053a61f976c
shhhQuiettt <krzysztof.skro@gmail.com>
2026-03-17 15:02:01 +0100

    Add Poly3d Trajectory

--- flc.py
+++ flc.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import pi
 
 from controllers.dummy_controller import DummyController
 from controllers.feedback_linearization_controller import (
@@ -10,7 +11,7 @@ from trajectory_generators.poly3 import Poly3
 from trajectory_generators.sinusonidal import Sinusoidal
 from utils.simulation import simulate
 
-
+import sys
 
 Tp = 0.01
 start = 0
@@ -26,10 +27,10 @@ controller = FeedbackLinearizationController(Tp)
 Here you have some trajectory generators. You can use them to check your implementations.
 At the end implement Point2point trajectory generator to move your manipulator to some desired state.
 """
-# traj_gen = ConstantTorque(np.array([0., 1.0])[:, np.newaxis])
-traj_gen = Sinusoidal(np.array([0., 1.]), np.array([2., 2.]), np.array([0., 0.]))
-#traj_gen = Poly3(np.array([0., 0.]), np.array([pi/4, pi/6]), end)
-
+# traj_gen = ConstantTorque(np.array([0., 100.0]))#[:, np.newaxis])
+# traj_gen = Sinusoidal(np.array([0., 1.]), np.array([2., 2.]), np.array([0., 0.]))
+traj_gen = Poly3(np.array([0.0, 0.0]), np.array([pi / 4, pi / 6]), end)
+# traj_gen.plot(100)
 
 Q, Q_d, u, T = simulate("PYBULLET", traj_gen, controller, Tp, end)
 
@@ -40,21 +41,20 @@ with respect to time 'T' to analyze what is going on in the system
 """
 plt.subplot(221)
 plt.title("q1 and q1_d")
-plt.plot(T, Q[:, 0], 'r')
-plt.plot(T, Q_d[:, 0], 'b')
-plt.legend(['q1', 'q1_d'])
+plt.plot(T, Q[:, 0], "r")
+plt.plot(T, Q_d[:, 0], "b")
+plt.legend(["q1", "q1_d"])
 
 plt.subplot(222)
 plt.title("q2 and q2_d")
-plt.plot(T, Q[:, 1], 'r')
-plt.plot(T, Q_d[:, 1], 'b')
-plt.legend(['q2', 'q2_d'])
-
+plt.plot(T, Q[:, 1], "r")
+plt.plot(T, Q_d[:, 1], "b")
+plt.legend(["q2", "q2_d"])
 
 
 plt.subplot(223)
 plt.title("u1 and u2")
-plt.plot(T, u[:, 0], 'r')
-plt.plot(T, u[:, 1], 'b')
-plt.legend(['u1', 'u2'])
+plt.plot(T, u[:, 0], "r")
+plt.plot(T, u[:, 1], "b")
+plt.legend(["u1", "u2"])
 plt.show()
--- trajectory_generators/poly3.py
+++ trajectory_generators/poly3.py
@@ -7,14 +7,33 @@ class Poly3(TrajectoryGenerator):
         self.T = T
         self.q_0 = start_q
         self.q_k = desired_q
+
+        assert self.q_0.shape == (2,) and self.q_k.shape == (2,)
         """
         Please implement the formulas for a_0 till a_3 using self.q_0 and self.q_k
         Assume that the velocities at start and end are zero.
         """
-        self.a_0 = None
-        self.a_1 = None
-        self.a_2 = None
-        self.a_3 = None
+        self.q_dot_0 = np.zeros_like(self.q_0)
+        self.q_dot_k = np.zeros_like(self.q_k)
+
+        matrix = np.array(
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+                [-3.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, -1.0, 3.0],
+            ]
+        )
+        y = np.vstack([self.q_0, self.q_k, self.q_dot_0, self.q_dot_k])
+
+        try:
+            matrix_inv = np.linalg.inv(matrix)
+        except np.linalg.LinAlgError:
+            raise ValueError(
+                f"Failed to create a polynomial trajectory generator for the given parameters: {self.q_0=}, {self.q_k=}, {self.T=}, {self.q_dot_0=}, {self.q_dot_k=}. The matrix {matrix} is singular."
+            )
+
+        self.a_0, self.a_1, self.a_2, self.a_3 = matrix_inv @ y
 
     def generate(self, t):
         """
@@ -24,7 +43,59 @@ class Poly3(TrajectoryGenerator):
         Use following formula for the polynomial from the instruction.
         """
         t /= self.T
-        q = self.a_3 * t**3 + self.a_2 * t**2 * (1 - t) + self.a_1 * t * (1 - t)**2 + self.a_0 * (1 - t)**3
-        q_dot = None
-        q_ddot = None
+        q = (
+            self.a_3 * t**3
+            + self.a_2 * t**2 * (1 - t)
+            + self.a_1 * t * (1 - t) ** 2
+            + self.a_0 * (1 - t) ** 3
+        )
+        q_dot = (
+            -3 * self.a_0 * (1 - t) ** 2
+            + self.a_1 * ((1 - t) ** 2 - 2 * t * (1 - t))
+            + self.a_2 * (2 * t * (1 - t) - t**2)
+            + 3 * self.a_3 * t**2
+        )
+        q_ddot = (
+            6 * self.a_0 * (1 - t)
+            + self.a_1 * (6 * t - 2)
+            + self.a_2 * (2 - 6 * t)
+            + 6 * self.a_3 * t
+        )
         return q, q_dot / self.T, q_ddot / self.T**2
+
+    def plot(self, time_steps):
+        import matplotlib.pyplot as plt
+
+        # q is 2 dimentional
+
+        t = np.linspace(0, self.T, time_steps)
+        q, q_dot, q_ddot = [], [], []
+        for time in t:
+            q_i, q_dot_i, q_ddot_i = self.generate(time)
+            q.append(q_i)
+            q_dot.append(q_dot_i)
+            q_ddot.append(q_ddot_i)
+        q = np.array(q)
+        q_dot = np.array(q_dot)
+        q_ddot = np.array(q_ddot)
+
+        plt.figure(figsize=(12, 8))
+        plt.subplot(311)
+        plt.title("Position")
+        plt.plot(t, q[:, 0], label="q1")
+        plt.plot(t, q[:, 1], label="q2")
+        plt.legend()
+
+        plt.subplot(312)
+        plt.title("Velocity")
+        plt.plot(t, q_dot[:, 0], label="q1_dot")
+        plt.plot(t, q_dot[:, 1], label="q2_dot")
+        plt.legend()
+
+        plt.subplot(313)
+        plt.title("Acceleration")
+        plt.plot(t, q_ddot[:, 0], label="q1_ddot")
+        plt.plot(t, q_ddot[:, 1], label="q2_ddot")
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
